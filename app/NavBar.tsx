2aeadafdb815eca909bff4eac94b5a61a82dfaaa
Aditya060698 <mishraditya06@gmail.com>
2024-10-31 20:54:52 +0530

    Active Link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBugSharp } from "react-icons/io5";
+import classnames from 'classnames';
 
 function NavBar() {
+  const currentPath=usePathname()  
   const Links = [
     { id: 0, labels: "Dasboard", href: "/" },
     { id: 1, labels: "Issues", href: "/issues" },
@@ -12,7 +16,11 @@ function NavBar() {
       <Link href="/"><IoBugSharp/></Link>
       <ul className='flex space-x-6'>
         {Links.map((item) => (
-          <li key={item.id} className="text-zinc-500 hover:text-zinc-900 transition-colors">
+          <li key={item.id} className={classnames({
+            'text-zinc-500':currentPath !== item.href,
+            'text-zinc-950':currentPath === item.href,
+            'hover:text-zinc-900 transition-colors':true
+          })}>
             <Link href={item.href}>{item.labels}</Link>
           </li>
         ))}
